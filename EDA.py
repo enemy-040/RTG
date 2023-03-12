f82328b215dc0be4c87a19dcc06b8650b802e4aa
stilxam <m.l.h.litsios@student.tue.nl>
2023-03-12 17:20:00 +0100

    need to extract predictions from ARCH models

--- EDA.py
+++ EDA.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import Tuple
 import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-#from arch import arch_model
+from arch import arch_model
 from sklearn.model_selection import train_test_split
 
 
@@ -51,15 +51,15 @@ book = pd.read_json('data/books.json').values
 
 ask_Std_Arr, ask_mn_Arr, bid_Std_Arr, bid_mn_Arr = getMnVarAtTime(book)
 
-plt.plot(bid_Std_Arr)
-plt.plot(ask_Std_Arr)
-plt.savefig('stdvPlt.png')
+#plt.plot(bid_Std_Arr)
+#plt.plot(ask_Std_Arr)
+#plt.savefig('stdvPlt.png')
 
-plot_acf(bid_Std_Arr)
-plot_pacf(bid_Std_Arr)
+#plot_acf(bid_Std_Arr)
+#plot_pacf(bid_Std_Arr)
 
-plot_acf(ask_Std_Arr)
-plot_pacf(ask_Std_Arr)
+#plot_acf(ask_Std_Arr)
+#plot_pacf(ask_Std_Arr)
 
 ask_std_train, ask_std_test, bid_std_train, bid_std_test = train_test_split(
         ask_Std_Arr,
@@ -69,10 +69,77 @@ ask_std_train, ask_std_test, bid_std_train, bid_std_test = train_test_split(
         random_state=69
         )
 
-#model = arch_model(train, mean='Zero', vol='GARCH', p=6, q=#tbd take variance of variance)
-exp_var = {}
-for i in range(1, 10):
-    exp_var[i] = pd.Series(ask_std_train).rolling(window=i+2).var()
 
-print(exp_var)
+ask_model = arch_model(
+            ask_std_train,
+            vol='ARCH',
+            p=6,
+            ).fit(disp='off')
+
+bid_model = arch_model(
+        bid_std_train,
+        vol='ARCH',
+        p=6).fit(disp='off')
+g_ask_models = {}
+g_bid_models = {}
+
+for i in range(1, 10):
+    g_ask_models[i] = arch_model(
+            ask_std_train,
+            vol='GARCH',
+            p=6,
+            q=i
+            ).fit(disp='off')
+    g_bid_models[i] = arch_model(
+            bid_std_train,
+            vol='GARCH',
+            p=6,
+            q=i
+            ).fit(disp='off')
+
+print(f"ask ARCH bic = {ask_model.bic}")
+
+ask_std_pred = ask_model.forecast(horizon=len(ask_std_test), reindex=True)
+print(ask_std_pred.mean)
+print(f"""ask ARCH MSE = {np.mean(
+    (ask_std_test - ask_std_pred)**2)}
+      """)
+
+print(f"bid ARCH bic = {bid_model.fit(disp='off').bic}")
+
+bid_std_pred = bid_model.forecast(horizon=len(ask_std_test))
+print(f"""bid ARCH MSE = {np.mean(
+    (bid_std_test - bid_std_pred)**2)}
+      """)
+
+g_ask_pred = {}
+g_bid_pred = {}
+for i in range(1, 10):
+    g_ask_pred[i] = g_ask_models[i].forecast(len(ask_std_test))
+    g_bid_pred[i] = g_bid_models[i].forecast(len(bid_std_test))
+
+    print(f'''q={i},
+          ask[bic = {g_ask_models[i].bic},
+              mse = {np.mean(
+    (ask_std_test - g_ask_pred[i])**2)}],
+          bid[bic = {g_bid_models[i].bic},
+              mse = {np.mean((
+                  bid_std_test - g_bid_pred[i])**2)}]''')
+
+
+
+
+#%%
+# maxRollingVar = 100
+# for i in range(maxRollingVar):
+#     exp_var[i] = pd.Series(ask_std_train).rolling(window=i+2).var()
+#
+# meanVar = np.empty(maxRollingVar)
+# for key in exp_var.keys():
+#     meanVar[key] = exp_var[key].mean()
+#     print(f'{key}: mean variance {meanVar[key]}')
+#%%
+
+
+# plt.plot(exp_var[99])
 
